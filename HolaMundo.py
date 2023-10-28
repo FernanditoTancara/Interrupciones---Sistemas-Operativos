73391a7d014af9b0bbd9c62d3214173014ae2a5b
FernanditoTancara <146409413+FernanditoTancara@users.noreply.github.com>
2023-10-28 05:26:12 -0500

    Update HolaMundo.py

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,52 +1,45 @@
-import time
 import random
-from threading import Thread
+import time
+from threading import Thread, Lock
 
-#Querarquia de Prioridades
-#1: Multiplicacion
-#2: Division
-#3: Suma
-#4 Resta
+class Proceso:
+    def __init__(self, nombre, valor):
+        self.nombre = nombre
+        self.valor = valor
+        self.mutex = Lock()
 
-def resta():
-    print("Resta iniciada")
-    time.sleep(random.uniform(0, 10))
-    print("Resta terminada")
-def suma():
-    print("Suma iniciada")
-    time.sleep(random.uniform(0, 10))
-    print("Suma terminada")
-def division():
-    print("División iniciada")
-    time.sleep(random.uniform(0, 10))
-    print("División terminada")
-def multiplicacion():
-    print("Multiplicación iniciada")
-    time.sleep(random.uniform(0, 10))
-    print("Multiplicación terminada")
+def ejecutar_proceso(proceso):
+    with proceso.mutex:
+        print(f"Proceso {proceso.nombre} iniciado")
+        tiempo_demora = random.uniform(1, 5)
+        time.sleep(tiempo_demora)
+        print(f"Proceso {proceso.nombre} terminado ({tiempo_demora:.2f} segundos)")
 
+def interrumpir(proceso1, proceso2):
+    print(f"Proceso {proceso1.nombre} interrumpió a Proceso {proceso2.nombre}")
 
-def ejecutar_proceso(proceso, prioridad):
-    start_time = time.time()
-    print(f"{proceso.__name__} iniciada (Prioridad: {prioridad})")
-    while time.time() - start_time < 5:
-        if random.random() < 0.1:  # Generar interrupción con 10% de probabilidad
-            interrumpir_otro_proceso(proceso.__name__, prioridad)
-            break
-        time.sleep(0.1)
-    print(f"{proceso.__name__} terminada")
+def main():
+    resta = Proceso("Resta", 1)
+    suma = Proceso("Suma", 2)
+    division = Proceso("Division", 3)
+    multiplicacion = Proceso("Multiplicacion", 4)
 
-def interrumpir_otro_proceso(proceso_actual, prioridad_actual):
-    procesos = [(resta, 1), (suma, 2), (division, 3), (multiplicacion, 4)]
-    procesos = [p for p in procesos if p[1] < prioridad_actual]  # Filtrar por prioridad menor
-    if procesos:
-        proceso_a_interrumpir, _ = random.choice(procesos)
-        print(f"{proceso_actual} interrumpió a {proceso_a_interrumpir}")
-        Thread(target=proceso_a_interrumpir).start()
-    else:
-        print(f"No hay procesos de menor prioridad para interrumpir la {proceso_actual}")
+    procesos = [resta, suma, division, multiplicacion]
 
-Thread(target=ejecutar_proceso, args=(resta, 1)).start()
-Thread(target=ejecutar_proceso, args=(suma, 2)).start()
-Thread(target=ejecutar_proceso, args=(division, 3)).start()
-Thread(target=ejecutar_proceso, args=(multiplicacion, 4)).start()
+    while True:
+        proceso1, proceso2 = random.sample(procesos, 2)
+        if proceso1.valor > proceso2.valor:
+            hilo_proceso1 = Thread(target=ejecutar_proceso, args=(proceso1,))
+            hilo_proceso2 = Thread(target=ejecutar_proceso, args=(proceso2,))
+
+            hilo_proceso1.start()
+            time.sleep(0.5)
+            hilo_proceso2.start()
+
+            hilo_proceso1.join()
+            hilo_proceso2.join()
+
+            interrumpir(proceso1, proceso2)
+
+if __name__ == "__main__":
+    main()
